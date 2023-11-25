5d5f01f5b6ad760c84db8cf5483247ad83cff288
hotsixisbest <babytaikoer>
2023-11-25 15:53:35 +0900

    asd

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -6,8 +6,8 @@
     {form?.code}
 {/if}
 <form method="post">
-    <input type="text" name="id">
-    <input type="password" name="password">
+    <input type="text" name="id" placeholder="아이디">
+    <input type="password" name="password" placeholder="비밀번호">
     <input type="submit">
 </form>
 
